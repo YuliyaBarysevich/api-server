50cff1a04dedf9bd10d158fc734c81e1d2312953
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-04-23 01:47:31 -0400

    fixed bug

--- index.js
+++ index.js
@@ -5,38 +5,13 @@ const dotenv = require('dotenv');
 const PORT = process.env.PORT || 3000;
 
 const mongoose = require('mongoose')
-const MONGDB_URI = 'mongodb://localhost:27017/food'
+const MONGODB_URI = 'mongodb://localhost:27017/food'
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose.connect(MONGODB_URI, options);
 
 
 dotenv.config();
 
 server.start(PORT)
 
-// const databaseInteractions = async () => {
-
-//   let pizza = {
-//     product: 'pizza',
-//     calories: 1200,
-//     price: 12
-//   }
-
-//   let apple = {
-//     product: 'apple',
-//     calories: 30,
-//     price: 5
-//   }
-
-//   let newFood = await food.create(pizza);
-//   let moreFood = await food.create(apple);
-
-//   console.log('create:', newFood);
-
-//   let oneFood = await food.read(newFood._id);
-//   console.log('get one food item', oneFood);
-
-//   let allFoods = await food.read();
-//   console.log(allFoods);
-// }
-
-// databaseInteractions();
